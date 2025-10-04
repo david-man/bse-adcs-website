152e9c9b92816583231fd873e07c0e88a9648a44
david-man <86388290+david-man@users.noreply.github.com>
2025-10-04 18:42:28 -0400

    .

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -16,12 +16,14 @@ function Help(){
         fetch(help_url, {
             method: 'POST',
             body: formData
+        }).then(() => {
+            navigator("home")
         })
     }
     
     return <div className = 'min-w-fit w-full h-full flex flex-col items-center justify-around'>
         <h1 className = 'text-3xl w-full text-center m-5'>Contact me!</h1>
-        <form action={help_url} method="POST" className = 'w-9/10 flex flex-col gap-[20px]'>
+        <form onSubmit = {() => {submit()}} className = 'w-9/10 flex flex-col gap-[20px]'>
             <div className = 'w-full flex'>
                 <p className = 'p-[3px] pr-[25px]'>Name: </p>
                 <input onChange = {(event) => setName(event.target.value)} type="text" name="name" className = 'min-w-[200px] w-3/10 border-2 rounded-2xl p-[5px]'></input>
@@ -36,10 +38,7 @@ function Help(){
             </div>
             
             <div className = 'flex w-full justify-center items-center'>
-                <button type='submit' onSubmit = {() => {
-                    submit()
-                    navigator("home")
-                    }} className = 'min-w-[50px] w-[1/10] border-2 rounded-2xl cursor-pointer'>Send</button>
+                <button type='submit' className = 'min-w-[50px] w-[1/10] border-2 rounded-2xl cursor-pointer'>Send</button>
             </div>
         </form>
     </div>
